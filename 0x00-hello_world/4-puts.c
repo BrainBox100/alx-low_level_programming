ca6f543426c1794110afba83d7156f7a50ddd190
Umar <yuzy4lyf@gmail.com>
2023-09-07 13:45:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  *  main - prints a given phrase
- *   *  Return: 0 if exited properly, non-zero otherwise
- *    */
+ *    main - prints a given phrase
+ *    Return: 0 if exited properly, non-zero otherwie
+  /
 
 int  main(void)
 {
