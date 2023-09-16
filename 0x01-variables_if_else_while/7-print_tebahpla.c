b692d1a106e66876325dcdfccbc3f4aeea9e5867
Umar <yuzy4lyf@gmail.com>
2023-09-16 06:03:19 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,18 +2,23 @@
 
 /**
  * main - main block
- * Description: Print the alphabet in reverse order in lowercase.
+ * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
  * Return: 0
  */
 
 int main(void)
 {
-	char c;
+	char c = 'z';
 
-	for (c = 'z'; c >= 'a'; c--)
+	while (c >= 'a')
 	{
 		putchar(c);
+		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
