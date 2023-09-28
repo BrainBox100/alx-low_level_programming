7dadaad33673b92469b576850ea4e714063fc677
Umar <yuzy4lyf@gmail.com>
2023-09-28 08:52:53 -0700

    added

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_reverse_recursion - reverse a string to stdout using recursion.
+ * @s: A pointer to the character array string.
+ * Description: This function recursively prints in
+ * reverse each character of the string
+ * pointed to by 's' until it reaches the null terminator.
+ *
+ * Return: None.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
