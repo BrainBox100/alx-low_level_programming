0a198acdabec4fffd9b6ff64ef1474cc5d38eb0f
Umar <yuzy4lyf@gmail.com>
2023-10-03 05:30:46 -0700

    add

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,8 @@
  * @argv: Array of pointers to the command-line arguments
  *
  * Description: This program prints the number of arguments passed into it.
- * Each argument is counted and the total number is printed, followed by a new line.
+ * Each argument is counted and the total number is printed
+ * followed by a new line
  *
  * Return: 0 if successful, non-zero otherwise
  */
