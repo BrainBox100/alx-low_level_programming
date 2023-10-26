439f36e174899a15a65ddf35f63ff320d09107aa
yuzy <yuzy4lyf@gmail.com>
2023-10-26 10:45:44 -0700

    add

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	fig = 0;
 	if (!b)
 		return (0);
-	for (k = 0; b[k] != '\0'; i++)
+	for (k = 0; b[k] != '\0'; k++)
 	{
 		if (b[k] != '0' && b[k] != '1')
 			return (0);
